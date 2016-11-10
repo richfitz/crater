680d97f763597fed9729cfc571a21e4eb8dc413c
Rich FitzJohn <rich.fitzjohn@gmail.com>
2016-11-10 16:09:49 +0000

    Remove old code

--- R/util.R
+++ R/util.R
@@ -1,11 +1,3 @@
-## Temporary stopgap, until I decide if httr will be kept
-http_method <- function(type) {
-  switch(type,
-         GET = httr::GET,
-         POST = httr::GET,
-         stop("Invalid method ", type))
-}
-
 `%||%` <- function(a, b) {
   if (is.null(a)) b else a
 }
